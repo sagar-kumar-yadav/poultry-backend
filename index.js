329da7ec002575198727a18bbf7b5297f01e80c9
sagar-kumar-yadav <sk5323912@gmail.com>
2024-11-06 22:22:39 +0530

    cors error

--- index.js
+++ index.js
@@ -18,26 +18,17 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://poultry-backend-pdpi.onrender.com",
+  "https://aadarshudyoug.netlify.app"
 ];
 
 // middleware
 app.use(
   cors({
     origin: allowedOrigins,
-    
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "React app URL"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 
 app.use(express.json());
 app.use(morgan("dev"));
